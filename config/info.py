d1be171c9e92bb18577d46788b70c3c923f5d011
cipix-cmd <misterpig.2055@gmail.com>
2024-07-24 14:54:02 +0200

    Info.py

---
+++ config/info.py
@@ -0,0 +1,15 @@
+# © 2024 CIPIX
+# All rights reserved.
+# Tous droits réservés.
+
+# General Information 
+creator = 'Cipix'
+versionCode = '0.1'
+# Link
+linkDiscord = 'https://discord.gg/pZSVzVkXBR'
+linkGithub = 'https://github.com/CIPIX-off/CodeBreak'
+linkUpdate = 'https://raw.githubusercontent.com/CIPIX-off/CodeBreak/main/config/info.py'
+# Webhooks
+webhookName = 'CodeBreak'
+webhookColor = 0x8300FF
+webhookPP = 'https://www.wattlecorp.com/wp-content/uploads/2020/09/redhat-hackers-cybersecurity-ethicalhacking-kerala-wattlecorp-1024x1024.png'
